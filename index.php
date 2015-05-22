804111d1b265e281ccae5f601220513cda4a54ae
Shaun Ready <readysd@gmail.com>
2015-05-22 14:58:37 -0300

    - Moved dashboard to new file - Added expandable tables with info from database

--- index.php
+++ index.php
@@ -3,20 +3,33 @@
   $db_host = 'test-rds.cpnv7me8ryeg.us-east-1.rds.amazonaws.com';
   $db_username = 'shaun';
   $db_password = 'Virmire-209';
-  $db_name = 'test-db';
+  $db_table = "test_db_2";
 
-  $db = new mysqli($db_host, $db_username, $db_password);
+  $db = new mysqli($db_host, $db_username, $db_password, $db_table);
 
   if ($db->connect_errno) {
     echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error;
   }
 
-  if (!$db->select_db('test_db')) {
-    $db->query("CREATE DATABASE test_db");
-    $db->select_db('test_db');
-  }
+  $sectionName = "Acute Care";
 
+  $result = $db->query("SELECT ID
+                        FROM Sections
+                        WHERE Name = '" . $sectionName . "'");
+
+  $sectionID = (int)$result->fetch_assoc()['ID'];
+
+  $subSections = $db->query(" SELECT s.*, sr.*
+                              FROM Sections s
+                                INNER JOIN SectionRelationships sr ON s.ID = sr.RelativeSectionID
+                              WHERE sr.SectionID = " . $sectionID . " AND sr.RelativeIsChild = 1
+                            ");
+
+  $events = $db->query("  SELECT *
+                          FROM " . str_replace(' ', '', $sectionName) . "Events
+                      ");
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -28,17 +41,13 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Dashboard Template for Bootstrap</title>
+    <title>TactiCal Test</title>
 
     <!-- Bootstrap core CSS -->
     <link href="http://getbootstrap.com/dist/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <link href="http://getbootstrap.com/examples/dashboard/dashboard.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -48,210 +57,96 @@
   </head>
 <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="#">Dashboard</a></li>
-            <li><a href="#">Settings</a></li>
-            <li><a href="#">Profile</a></li>
-            <li><a href="#">Help</a></li>
-          </ul>
-          <form class="navbar-form navbar-right">
-            <input type="text" class="form-control" placeholder="Search...">
-          </form>
-        </div>
-      </div>
-    </nav>
 
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-sm-3 col-md-2 sidebar">
-          <ul class="nav nav-sidebar">
-            <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Reports</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-          </ul>
-        </div>
-        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header">Dashboard</h1>
 
-          <div class="row placeholders">
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-            <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
-              <h4>Label</h4>
-              <span class="text-muted">Something else</span>
-            </div>
-          </div>
+          
+          
+<div class="col-lg-6">
+  <div class="panel panel-default">
+    <div class="panel-heading"><h3>Sub Sections</h3></div>
+    <div class="panel-body">
+      <table class="table table-condensed" style="border-collapse:collapse;">
 
-          <h2 class="sub-header">Section title</h2>
-          <div class="table-responsive">
-            <table class="table table-striped">
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                  <th>Header</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>1,001</td>
-                  <td>Lorem</td>
-                  <td>ipsum</td>
-                  <td>dolor</td>
-                  <td>sit</td>
-                </tr>
-                <tr>
-                  <td>1,002</td>
-                  <td>amet</td>
-                  <td>consectetur</td>
-                  <td>adipiscing</td>
-                  <td>elit</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>Integer</td>
-                  <td>nec</td>
-                  <td>odio</td>
-                  <td>Praesent</td>
-                </tr>
-                <tr>
-                  <td>1,003</td>
-                  <td>libero</td>
-                  <td>Sed</td>
-                  <td>cursus</td>
-                  <td>ante</td>
-                </tr>
-                <tr>
-                  <td>1,004</td>
-                  <td>dapibus</td>
-                  <td>diam</td>
-                  <td>Sed</td>
-                  <td>nisi</td>
-                </tr>
-                <tr>
-                  <td>1,005</td>
-                  <td>Nulla</td>
-                  <td>quis</td>
-                  <td>sem</td>
-                  <td>at</td>
-                </tr>
-                <tr>
-                  <td>1,006</td>
-                  <td>nibh</td>
-                  <td>elementum</td>
-                  <td>imperdiet</td>
-                  <td>Duis</td>
-                </tr>
-                <tr>
-                  <td>1,007</td>
-                  <td>sagittis</td>
-                  <td>ipsum</td>
-                  <td>Praesent</td>
-                  <td>mauris</td>
-                </tr>
-                <tr>
-                  <td>1,008</td>
-                  <td>Fusce</td>
-                  <td>nec</td>
-                  <td>tellus</td>
-                  <td>sed</td>
-                </tr>
-                <tr>
-                  <td>1,009</td>
-                  <td>augue</td>
-                  <td>semper</td>
-                  <td>porta</td>
-                  <td>Mauris</td>
-                </tr>
-                <tr>
-                  <td>1,010</td>
-                  <td>massa</td>
-                  <td>Vestibulum</td>
-                  <td>lacinia</td>
-                  <td>arcu</td>
-                </tr>
-                <tr>
-                  <td>1,011</td>
-                  <td>eget</td>
-                  <td>nulla</td>
-                  <td>Class</td>
-                  <td>aptent</td>
-                </tr>
-                <tr>
-                  <td>1,012</td>
-                  <td>taciti</td>
-                  <td>sociosqu</td>
-                  <td>ad</td>
-                  <td>litora</td>
-                </tr>
-                <tr>
-                  <td>1,013</td>
-                  <td>torquent</td>
-                  <td>per</td>
-                  <td>conubia</td>
-                  <td>nostra</td>
-                </tr>
-                <tr>
-                  <td>1,014</td>
-                  <td>per</td>
-                  <td>inceptos</td>
-                  <td>himenaeos</td>
-                  <td>Curabitur</td>
-                </tr>
-                <tr>
-                  <td>1,015</td>
-                  <td>sodales</td>
-                  <td>ligula</td>
-                  <td>in</td>
-                  <td>libero</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+        <thead>
+          <tr><th>&nbsp;</th>
+            <th>Sub Section</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php
+            $i = 0;
+            foreach ($subSections as $row) {
+              echo "<tr data-toggle='collapse' data-target='#test" . $i . "' class='accordion-toggle'>
+                      <td><button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-eye-open'></span></button></td>
+                      <td>" . $row['Name'] . "</td>
+                    </tr>
+                    <tr>
+                      <td colspan='12' class='hiddenRow'><div class='accordian-body collapse' id='test" . $i++ . "'> 
+                        <table class='table table-striped'>
+                          <thead>
+                            <tr><td><a href='WorkloadURL'>Workload link</a></td><td>Bandwidth: Dandwidth Details</td><td>OBS Endpoint: end point</td></tr>
+                            <tr><th>Access Key</th><th>Secret Key</th><th>Status </th><th> Created</th><th> Expires</th><th>Actions</th></tr>
+                          </thead>
+                          <tbody>
+                            <tr><td>access-key-1</td><td>secretKey-1</td><td>Status</td><td>some date</td><td>some date</td><td><a href='#' class='btn btn-default btn-sm'>
+                            <i class='glyphicon glyphicon-cog'></i></a></td></tr>
+                          </tbody>
+                        </table>            
+                    </div></td>
+                    </tr>";                  
+            }
+          ?>
+
+        </tbody>
+      </table>
     </div>
+  </div> 
+</div>
+
+<div class="col-lg-6">
+  <div class="panel panel-default">
+    <div class="panel-heading"><h3>Events</h3></div>
+    <div class="panel-body">
+      <table class="table table-condensed" style="border-collapse:collapse;">
+
+        <thead>
+          <tr><th>&nbsp;</th>
+            <th>Sub Section</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <?php
+            foreach ($events as $row) {
+              echo "<tr data-toggle='collapse' data-target='#test" . $i . "' class='accordion-toggle'>
+                      <td><button class='btn btn-default btn-xs'><span class='glyphicon glyphicon-eye-open'></span></button></td>
+                      <td>" . $row['EventName'] . "</td>
+                    </tr>
+                    <tr>
+                      <td colspan='12' class='hiddenRow'><div class='accordian-body collapse' id='test" . $i++ . "'> 
+                        <table class='table table-striped'>
+                          <thead>
+                            <tr><td><a href='WorkloadURL'>Workload link</a></td><td>Bandwidth: Dandwidth Details</td><td>OBS Endpoint: end point</td></tr>
+                            <tr><th>Access Key</th><th>Secret Key</th><th>Status </th><th> Created</th><th> Expires</th><th>Actions</th></tr>
+                          </thead>
+                          <tbody>
+                            <tr><td>access-key-1</td><td>secretKey-1</td><td>Status</td><td>some date</td><td>some date</td><td><a href='#' class='btn btn-default btn-sm'>
+                            <i class='glyphicon glyphicon-cog'></i></a></td></tr>
+                          </tbody>
+                        </table>            
+                    </div></td>
+                    </tr>";                  
+            }
+          ?>
+
+        </tbody>
+      </table>
+    </div>
+  </div> 
+</div>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="http://getbootstrap.com/dist/js/bootstrap.min.js"></script>
