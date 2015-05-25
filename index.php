5e00ada368b559ae29d2286d2386b2f2916c09db
Shaun Ready <readysd@gmail.com>
2015-05-25 09:51:57 -0300

    4

--- index.php
+++ index.php
@@ -17,17 +17,6 @@ include '/srv/www/test_app/shared/config/opsworks.php';
                             FROM Sections
                          ");
 
-  $subSections = $db->query(" SELECT s.*, sr.*
-                              FROM Sections s
-                                INNER JOIN SectionRelationships sr ON s.ID = sr.ChildSectionID
-                              WHERE sr.SectionID = " . $sectionID
-                            );
-
-
-  $events = $db->query("  SELECT *
-                          FROM " . str_replace(' ', '', $sectionName) . "Events
-                      ");
-
 ?>
 
 <!DOCTYPE html>
